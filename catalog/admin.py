e734d0e3deafc85c874795712e459a068915c7c5
Mikias <mikias_abiy@proton.me>
2025-05-24 12:55:33 +0300

    update: catalog/admin.py:  Admin site model representation for catalog app  updated

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Book, Author, Genre, BookInstance
 
-# Register your models here.
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 3
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back')
+    list_filter = ('status', 'due_back') 
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')            
+        }),
+        ('Availablity', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+
+admin.site.register(Genre)
